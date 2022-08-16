ab187f20c0483c46dff0746cc4f218fdcb273599
sumonaliupworke <sumonaliupwork@gmail.comm>
2022-08-16 00:19:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+// connect to server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bu6kaai.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db("volunteer");
+        const userCollection = database.collection("services");
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const query = userCollection.find({});
+            const services = await query.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+
+} run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from bangladesh');
+});
+app.listen(port, () => {
+    console.log('Listening the port', port);
+});
